2f022ff5202ff7ef7d0e986c9045daf43fabbb73
Dan <xeonchambies@gmail.com>
2022-03-28 19:56:56 -0500

    initial

---
+++ main.py
@@ -0,0 +1,20 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def Calculator():
+
+    operation = input("Enter Operation (ex. 5 + 9): ")
+
+
+
+
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    Calculator()
+
