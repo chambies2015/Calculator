dfdf528a9e81ef8286842facaff29ae4439270a8
Dan <xeonchambies@gmail.com>
2022-03-29 17:06:11 -0500

    better flexibility and repetition if desired.

--- main.py
+++ main.py
@@ -2,48 +2,90 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import time
 
+continueFlag = True
 
 def Calculator():
-
-    # numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-    # operators = ["-", "+", "/", "x", "X", "%", "*", "^"]
-    operandOne = int(input("Enter first operand: "))
-    operator = input("What Operator? (ex. +, -, *, /, %): ")
-    operandTwo = int(input("Enter second operand: "))
-
-    match operator:
-        case "-":
-            total = operandOne - operandTwo
-            print("Result: ", total)
-        case "+":
-            total = operandOne + operandTwo
-            print("Result: ", total)
-        case "/":
-            if operandTwo == 0:
-                print("Can't divide by zero. Try again.")
-                exit(Calculator())
-            else:
-                total = operandOne / operandTwo
-                print("Result: ", total)
-        case "*":
-            total = operandOne * operandTwo
-            print("Result: ", total)
-        case "%":
-            total = operandOne % operandTwo
-            print("Result: ", total)
+    continueFlag = True
+    while continueFlag:
+        Main_Menu()
 
 
-def Main():
-    flag = True
-    while flag:
-        Calculator()
-        repeat = input("Would you like to perform another operation? (y or n):  ")
-        if repeat == "n":
-            flag = False
+def Subtraction(num1, num2):
+    return int(num1) - int(num2)
+
+def Addition(num1, num2):
+    return int(num1) + int(num2)
+
+def Multiplication(num1, num2):
+    return int(num1) * int(num2)
+
+def Division(num1, num2):
+    return int(num1) / int(num2)
+
+def Modulus(num1, num2):
+    return int(num1) % int(num2)
+
+def Main_Menu():
+    global continueFlag
+    validInputs = ["+", "-", "/", "*", "%", "q", "Q"]
+    print("Welcome to Chambies' Calculator Main Menu!\nPlease make a selection below:\n"
+          "______________________________")
+    print("'+' for Addition\n'-' for Subtraction\n'*' for Multiplication\n'/' for Division\n"
+          "'%' for Modulus\n 'Q' for exiting program")
+    operator = input("Chosen Operator: ")
+    if operator not in validInputs:
+        print("Invalid input, Try again... \n")
+    else:
+        match operator:
+            case "q":
+                print("Exiting...")
+                continueFlag = False
+                quit()
+            case "Q":
+                print("Exiting...")
+                continueFlag = False
+                quit()
+
+        num1 = input("Your first number: ")
+        num2 = input("Your second number: ")
+
+        match operator:
+            case "+":
+                print("Result: ", Addition(num1, num2))
+                time.sleep(3)
+                print("Returning to menu...\n\n")
+                time.sleep(2)
+            case "-":
+                print("Result: ", Subtraction(num1, num2))
+                time.sleep(3)
+                print("Returning to menu...\n\n")
+                time.sleep(2)
+            case "*":
+                print("Result: ", Multiplication(num1, num2))
+                time.sleep(3)
+                print("Returning to menu...\n\n")
+                time.sleep(2)
+            case "/":
+                if num2 == '0':
+                    print("Can't divide by 0, Try again...")
+                    time.sleep(1)
+                    print("Returning to menu...\n\n")
+                    time.sleep(2)
+                else:
+                    print("Result: ", Division(num1, num2))
+                    time.sleep(3)
+                    print("Returning to menu...\n\n")
+                    time.sleep(2)
+            case "%":
+                print("Result: ", Modulus(num1, num2))
+                time.sleep(3)
+                print("Returning to menu...\n\n")
+                time.sleep(2)
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    Main()
+    Calculator()
 
