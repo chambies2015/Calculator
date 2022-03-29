7cba0edb7749d20cf8631677a1fa227faae29043
Dan <xeonchambies@gmail.com>
2022-03-28 20:37:53 -0500

    basic implementation

--- main.py
+++ main.py
@@ -6,15 +6,44 @@
 
 def Calculator():
 
-    operation = input("Enter Operation (ex. 5 + 9): ")
+    # numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    # operators = ["-", "+", "/", "x", "X", "%", "*", "^"]
+    operandOne = int(input("Enter first operand: "))
+    operator = input("What Operator? (ex. +, -, *, /, %): ")
+    operandTwo = int(input("Enter second operand: "))
+
+    match operator:
+        case "-":
+            total = operandOne - operandTwo
+            print("Result: ", total)
+        case "+":
+            total = operandOne + operandTwo
+            print("Result: ", total)
+        case "/":
+            if operandTwo == 0:
+                print("Can't divide by zero. Try again.")
+                exit(Calculator())
+            else:
+                total = operandOne / operandTwo
+                print("Result: ", total)
+        case "*":
+            total = operandOne * operandTwo
+            print("Result: ", total)
+        case "%":
+            total = operandOne % operandTwo
+            print("Result: ", total)
 
 
-
-
-
+def Main():
+    flag = True
+    while flag:
+        Calculator()
+        repeat = input("Would you like to perform another operation? (y or n):  ")
+        if repeat == "n":
+            flag = False
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    Calculator()
+    Main()
 
